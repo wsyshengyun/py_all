72b5932ce39514fc59c471ec021624e4cee96a7e
wsy <wsy8718@163.com>
2021-11-11 22:59:38 +0800

    移动py_base分支的dict相关到本分支

---
+++ base/defaultdict_some.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+'''
+@File    :   defaultdict_some.py
+@Time    :   2020/09/23 16:44:06
+'''
+from collections import defaultdict
+
+#=====================================统计几个key出现过那几个value=======================================
+s = [('yellow', 1), ('blue', 2), ('yellow', 3), ('red', 1), ('blue',4)]
+d = defaultdict(list)
+for k,v in s:
+    d[k].append(v)
+print(d)
+
+
+#=====================================统计一个key出现过多少次=======================================
+s = 'mississippi'
+d = defaultdict(int)
+for k in s:
+    d[k] += 1
+print(d)
+    
+#=====================================some=======================================
+d = defaultdict(list)
+d['a'].append(1)
+d['a'].append(2)
+d['b'].append(4)
+print(d)
+
+d = defaultdict(set)
+d['a'].add(1)
+d['a'].add(2)
+d['b'].add(4)
+print(d)
+
+
