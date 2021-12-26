f394b338f758329279e872c7af19a609491d869f
wsy <wsy8718@163.com>
2021-12-26 22:39:53 +0800

    python 循环导入测试

---
+++ base_test/import_test/A/__init__.py
@@ -0,0 +1,10 @@
+print("__init__ start")
+# def foo(name):
+#     print("foo() function: {}".format(name))
+#     return 2
+
+# app = foo
+
+# from A import a
+
+print("__init__ end")
---
+++ base_test/import_test/main.py
@@ -0,0 +1,6 @@
+print("main module start")
+from A.b import app
+
+app(__name__)
+
+print("main module end")
