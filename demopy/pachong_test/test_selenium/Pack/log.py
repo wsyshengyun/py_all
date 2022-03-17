8ce8a71bf4d89f7a486d669190f1a7360744dd64
wsy <wsy8718@163.com>
2022-03-17 16:18:28 +0800

    pyQt study!

--- demopy/pachong_test/test_selenium/Pack/log.py
+++
@@ -1,9 +0,0 @@
-import logging
-path_log = "Log/log1.log"
-level = logging.INFO
-logging.basicConfig(level=level
-        , format= '%(asctime)s - %(name)s - %(levelname)s : %(message)s'
-        , filename= path_log
-        , datefmt= '%Y/%m/%d %H:%M:%S'
-         )
-logger = logging.getLogger(__name__)
--- demopy/pachong_test/test_selenium/main.py
+++
@@ -1,1 +0,0 @@
-import Pack.Ip.ping
