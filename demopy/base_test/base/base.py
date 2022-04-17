0ba1d3b4912a056a26c66b808524279ad2278a47
wsy <wsy8718@163.com>
2022-04-18 07:39:28 +0800

    小的修改

--- demopy/base_test/base/base.py
+++ demopy/base_test/base/base.py
@@ -8,9 +8,14 @@
 import sys 
 
 x = '10' 
-b = '16' 
-print(int(x, 16))
-print(int(b, 16))
+b = '16'
+a = 10
+bin_tsr = bin(a) 
+hex_str = hex(a)
+oct_str = oct(a)
+print(int(bin_tsr,  2))   # 10 整数
+print(int(hex_str,  16))  # 10 整数
+print(int(oct_str,  8))  # 10 整数
 print("0x16打印出来为： %s" % 0x16)
 print("hex(16): %s" % hex(16))
 print(" int('10', 10): %s" % int('10', 10))
