7f6b61496e2748364913c349f8b3e0dd79539097
wsy <wsy8718@163.com>
2022-04-18 23:01:44 +0800

    修改了一些

--- demopy/base_test/function/some.py
+++ demopy/base_test/function/some.py
@@ -6,9 +6,9 @@
 
 from functools import wraps
 
+
 def fun():
     return id(fun)
-
 
 
 def logfunc(func):
@@ -16,8 +16,29 @@ def logfunc(func):
     def _inner(*args, **kwargs):
         return fun()
         # return 5
+
     return _inner
 
 
 print(fun())
 print(logfunc(fun)())
+
+
+# 函数签名
+def myfoo(a: 'int>0' = 80) -> str:
+    """ """
+    m = a + 100
+    return str(m)
+
+
+print(myfoo(-90))
+
+
+# 仅限关键字参数
+def foo(a, *, b):
+    print("a is {} b is {}".format(a, b))
+
+
+# foo(10, 20)  # 报错
+foo(10, b=20)  # 运行ok
+foo(b=10, a=20)  # 运行ok
