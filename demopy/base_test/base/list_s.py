cbafb6ee1a7767a5bc6f97e060a82989f182b23b
wsy <wsy8718@163.com>
2022-05-01 17:38:38 +0800

    list modify

--- demopy/base_test/base/list_s.py
+++ demopy/base_test/base/list_s.py
@@ -15,12 +15,10 @@ l.append(1)  # 在列表之后添加元素
 l.insert(1, 'insert at 1')  # 新添加的元素的位置在 索引1处，即在位置0后添加新元素
 
 # 扩展列表
-""" 
-list.extend(iterable) 参数为iterable类型
-在原来的列表上扩展；
-输出：为None
-添加的列表的元素在最后面；
-"""
+# list.extend(iterable) 参数为iterable类型
+# 在原来的列表上扩展；
+# 输出：为None
+# 添加的列表的元素在最后面；
 lit = ['a', 'b']
 lit = lit.extend(['x', 'y', 'z'])
 lit.extend('mnk')
@@ -34,14 +32,9 @@ lit.pop(0)  # 弹出第1个元素
 
 lit.pop()  # 弹出最后一个元素
 
-
-
 lit.remove(1)  # 删除元素1，是值不是索引；如果有删除的元素有重复，则只会删除最前面的；
 
-
-
 del lit[2]  # 删除索引为2的元素，也即删除从前到后第3个元素
-
 
 # 统计元素的个数方式
 print('x的个数为：', lit.count('x'))
