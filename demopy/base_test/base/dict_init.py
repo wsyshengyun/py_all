3cde4619e368c24262752c5cfbea7adc00e0a6b5
wsy <wsy8718@163.com>
2022-06-04 08:24:57 +0800

    字典的修改

--- demopy/base_test/base/dict_init.py
+++ demopy/base_test/base/dict_init.py
@@ -4,36 +4,25 @@
 # 创建空字典
 d1 = dict()
 d3 = dict(a=1, b=2, c='c')
-d1_1 = {}  # 速度比dict()快
-d2 = {'a': 1, 'b': 2}
+d4 = dict(d3, e=3) # dict + key
+d5 = dict((('name', 'jonah'), ('age', 14)))
+d5_ = dict(zip(['a', 'b', 'c'], [1, 2, 3, ])) # zip 方式
+d6 = d5_.copy()  # copy 方式
+d7 = dict(map(lambda x, y: (x, y), 'ABCD', range(1, 5)))  # map方式
+
+
+d11 = {}  # 速度比dict()快
+d12 = {'a': 1, 'b': 2}
 # 生成式方式
-lit = [1, 2, 3, 4, 5]
-d4 = {str(i): i for i in lit}
-# 直接通过可迭代对象生成字典
-lit = [('name', 'jonah'), ('age', 14)]
-d5 = dict(lit)
-# fromkeys方式
+d13 = {str(i): i for i in range(1, 5)}  # 1, 4  左闭右开
+
+# fromkeys方式, 直接通过可迭代对象生成字典
 # 1, 有一个参数, , 这个参数为字典的键, 参数为一个可迭代对象, key的值为None
+d14 = dict.fromkeys(['a', 'b', 'c'])  # {'a': None, 'b': None, 'c': None}
+d16 = dict.fromkeys((i for i in [1, 2, 3]))  # {1: None, 2: None, 3: None}
 # 2, 有两个参数, 第一个参数为一个可迭代对象, 第二个参数为默认的值
-d6_1 = dict.fromkeys(['a', 'b', 'c'])  # {'a': None, 'b': None, 'c': None}
-d6 = dict.fromkeys(['a', 'b', 'c'], 5)  # {'a': 5, 'b': 5, 'c': 5}
-g1 = (i for i in [1, 2, 3])  # 生成器
-d62 = dict.fromkeys(g1)  # {1: None, 2: None, 3: None}
+d15 = dict.fromkeys(['a', 'b', 'c'], 5)  # {'a': 5, 'b': 5, 'c': 5}
 
-# dict + key
-d_ = {'a': 1, 'b': 2}
-d7 = dict(d_, c=3)
-
-# zip 方式
-d8 = dict(zip(['a', 'b', 'c'], [1, 2, 3, ]))
-
-# map 方式
-mp = map(lambda x, y: (x, y), 'ABCD', range(1, 5))
-d8_1 = dict(mp)
-
-# copy 方式
-d9 = d_.copy()
-assert d9 == {'a': 1, 'b': 2}
 
 # 修改字典的key或者value
 dit = {'a': 1, 'b': 2, 'c': 3}
@@ -45,27 +34,19 @@ assert result1 == 8
 assert result2 == 3
 
 # update
-# 字典参数
 dx = {'a': 1, 'b': 2, 'c': 3}
 db = {'d': 4, 'e': 5}
-dx.update(db)
-assert dx == {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}
-
-# 关键字参数
-dx.update(f=6)
+dx.update(db)  # 字典参数
+dx.update(f=6)  # 关键字参数
+dx.update(db, f=6, g=7)  # 混合参数
 assert dx.get('f') == 6
 
-# update 混合参数  dict与关键字参数 kwargs
-dx = {'a': 1, 'b': 2, 'c': 3}
-db = {'d': 4, 'e': 5}
-dx.update(db, f=6, g=7)
-assert dx.get('d') == 4
-assert dx.get('g') == 7
 
 # 字典的用途
 temp = '姓名:%(name)s,年龄:%(age)3.0f,籍贯:%(location)s'
 msg = {'name': 'JoJ', 'age': 23, 'location': '海拉尔群岛'}
 print(temp % msg)
+
 # 字典可以排序,在python3.6之后,但是之前不可以排序
 dict_1 = {str(i): i for i in range(5)}
 print(dict_1)
